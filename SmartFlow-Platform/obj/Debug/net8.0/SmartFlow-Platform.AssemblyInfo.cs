e17f785d97ffd98bcf1a771e87c8b8f9b8465dc3
SebasSilvaT <svsilvat@gmail.com>
2024-11-18 11:54:48 -0500

    commit errores

--- SmartFlow-Platform/obj/Debug/net8.0/SmartFlow-Platform.AssemblyInfo.cs
+++ SmartFlow-Platform/obj/Debug/net8.0/SmartFlow-Platform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartFlow-Platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e05fb56ff8c844382197d723de2fbbbc8cec882")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4d7f8f0d01ecb1ea65b499c1ef02228d5231d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartFlow-Platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartFlow-Platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
